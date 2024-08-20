c5f79ba3d2788ae31c3b3b2d709944d7bbcb2630
Yuriy Halenko <yhalenko@gmail.com>
2024-08-20 20:45:39 +0100

    Player animations added.

--- scripts/utils.py
+++ scripts/utils.py
@@ -13,3 +13,26 @@ def load_images(folder_path):
     for img_name in sorted(os.listdir(BASE_IMG_PATH + folder_path)):
         images.append(load_image(folder_path + '/' + img_name))
     return images
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True) -> None:
+        self.images = images
+        self.img_dur = img_dur
+        self.loop = loop
+        self.done = False
+        self.frame = 0
+    
+    def copy(self):
+        return Animation(self.images, self.img_dur, self.loop)
+    
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_dur * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_dur * len(self.images) - 1:
+                self.done = True
+
+    def img(self):
+        return self.images[int(self.frame / self.img_dur)]
+
